d9f39c4148aa5628a2c9b91aa0a1aa62c80fa176
YoloSan <veselu@gmail.com>
2021-05-27 17:23:53 +0300

    use generic/class-based views

--- polls/views.py
+++ polls/views.py
@@ -1,33 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
+
+from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # long version: template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list}
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    return render(request, 'polls/index.html', context)
-    # long version: return HttpResponse(template.render(context, request))
-
-
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {'question': question})
+    def get_queryset(self):
+        """ Return the last five published questions. """
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -40,6 +38,7 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice."
         })
     else:
+        # account for race condition using F()
         selected_choice.votes = F('votes') + 1
         selected_choice.save()
 
