60a772a949c11437157b3ec377772676c452fdd3
YoloSan <veselu@gmail.com>
2021-05-28 15:19:37 +0300

    customize Question and Choice list

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': [
+         'pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'question', 'votes')
+    list_filter = ['question']
+    search_fields = ['choice_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
